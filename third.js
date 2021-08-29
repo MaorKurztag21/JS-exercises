8d4163bad61ec610a323d4932f3cabace579b804
MaorKurztag21 <rnrwznr@gmail.com>
2021-08-29 16:12:26 +0300

    Third Mission

---
+++ third.js
@@ -0,0 +1,10 @@
+const number =prompt('choose a number');
+let absolute;
+// your code goes here
+if(number>=0){
+    absolute=number;
+}
+else{
+    absolute=number*(-1);
+}
+console.log(absolute);
